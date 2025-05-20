ff4c54b825aadf5e95448025972c7bfa8c54f38a
Shaili-Patel <526shailipatel@frhsd.com>
2025-05-20 11:54:22 -0400

    BASKETBALL Font

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const popup = document.getElementById("welcome-popup");
-    const closeBtn = document.getElementById("close-popup");
-
-    closeBtn.addEventListener("click", function () {
-        popup.style.display = "none";
-    });
-});
